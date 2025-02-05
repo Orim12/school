b8b45d178867626c206ea0d15cf79ac5eb92c008
Orim12 <miro.vaassen@gmail.com>
2025-02-05 11:00:13 +0100

    Update Program.cs to prompt for birth date and display current date

--- progameren/prog 5/opdracht 1/ConsoleApp1/Program.cs
+++ progameren/prog 5/opdracht 1/ConsoleApp1/Program.cs
@@ -1,9 +1,13 @@
-﻿namespace ConsoleApp1;
+﻿using System;
+
+namespace ConsoleApp1;
 
 class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello, World!");
+        Console.WriteLine("when were you born?");
+        var input = Console.ReadLine();
+        var date = DateTime.Now.ToString("dd-MM-yyyy");
     }
 }
