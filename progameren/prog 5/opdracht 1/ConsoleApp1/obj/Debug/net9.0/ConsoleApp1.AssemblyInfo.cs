10aeacf2c5e53ac516a41340346d8488fe6ee62d
Orim12 <miro.vaassen@gmail.com>
2025-02-10 10:28:03 +0100

    Update AssemblyInfo to reflect new version number

--- progameren/prog 5/opdracht 1/ConsoleApp1/obj/Debug/net9.0/ConsoleApp1.AssemblyInfo.cs
+++ progameren/prog 5/opdracht 1/ConsoleApp1/obj/Debug/net9.0/ConsoleApp1.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("ConsoleApp1")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+164546b408fdc8fafc514591359e5c4f66ea8c61")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+4a677c6c8aa61c9bcc140a58b83c6d8618a1100b")]
 [assembly: System.Reflection.AssemblyProductAttribute("ConsoleApp1")]
 [assembly: System.Reflection.AssemblyTitleAttribute("ConsoleApp1")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
