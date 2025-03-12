e721ca9e3476181930afbb704de56232720c10d6
Orim12 <miro.vaassen@gmail.com>
2025-03-12 10:25:00 +0100

    Fix indentation in Instrument class constructor for consistency

--- progameren/prog-5/les 3/instrument.cs
+++ progameren/prog-5/les 3/instrument.cs
@@ -2,7 +2,7 @@ namespace les_3;
 
 public class Instrument(string geluid)
 {
-            readonly string mijnGeluid = geluid;
+    readonly string mijnGeluid = geluid;
 
     public string SpeelMijnGeluid()
     {
